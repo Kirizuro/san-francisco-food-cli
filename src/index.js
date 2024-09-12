22dcff64198cb6332f5aefc42b9434ac0a1771e9
Claudio Junior <claudio.junior14@hotmail.com>
2024-09-12 17:45:33 -0300

    removed unnecessary envs and packages

--- src/index.js
+++ src/index.js
@@ -1,29 +1,7 @@
 import axios from "axios";
 
-import arg from "arg";
-
-const argumentsAsOptions = (rawArgs) => {
-  const args = arg({
-    "--help": Boolean,
-    "--type": String,
-    "--address": String,
-    "--name": String,
-    "-h": "--help",
-    "-t": "--type",
-    "-a": "--address",
-    "-n": "--name",
-  });
-
-  return {
-    type: args["--type"] || false,
-    address: args["--address"] || false,
-  };
-};
 const dataSF = axios.create({
   baseURL: `https://data.sfgov.org/resource/bbb8-hzi6.json`,
-  headers: {
-    "X-App-Token": "xUNyF0RUVGEN3GaiPFWw37xj1",
-  },
 });
 
 const getFoodTrucks = async () => {
@@ -31,26 +9,7 @@ const getFoodTrucks = async () => {
   return response.data;
 };
 
-const parseParams = (options) => {
-  if (!options.type && !options.address) {
-    return "";
-  }
-  console.log(options);
-
-  if (Object.keys(options).length === 1) {
-    return `?${Object.keys(options)}=${Object.values(options)}`;
-  }
-
-  return `?${options.type}=${options[options.type]}&${options.address}=${
-    options[options.address]
-  }`;
-};
-
 export const main = async () => {
-  // const options = argumentsAsOptions(process.argv);
-
-  // const params = parseParams(options);
-
   console.log("Getting food trucks...");
 
   const foodTrucks = await getFoodTrucks();
